e7a07c66ee75421e8e82b40d1c2c30c914f2b337
Advay Chandorkar <advay1612@icloud.com>
2026-02-09 09:35:57 -0500

    fix(leaderboard): remove unused React default import in LeaderboardPosition

--- src/components/LeaderboardPosition.tsx
+++ src/components/LeaderboardPosition.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, memo } from 'react'
+import { useState, useEffect, useRef, memo } from 'react'
 import './LeaderboardPosition.css'
 
 interface LeaderboardPositionProps {
